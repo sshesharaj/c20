6381fe3ec6a42254ca17b2b8726681c58b8c18b4
sshesharaj <82090385+sshesharaj@users.noreply.github.com>
2021-06-24 16:36:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+ var countDistanceX = 0;
+var gap = 60;
+
+
+
+
+
+
+
+
+function preload()
+{}
+
+function setup() {
+  createCanvas(9000, 668);
+for (var i = 0; i<24 ; i++) {
+  platform1=new Platform(countDistanceX);
+  countDistanceX = countDistanceX + platform1.rw
+
+gap = random([80,100,220,50,0])
+
+
+}
+  
+}
+
+function draw() {
+  background('skyblue');
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+ drawSprites();
+}
+
